674fdf03ca6569d965865c350d75ac0c9ca16a3e
KimMalsu <jsm4451@naver.com>
2022-07-06 21:50:36 +0900

    first commit

---
+++ backend/src/main/java/com/suminjin/simpleroom/SimpleroomApplication.java
@@ -0,0 +1,13 @@
+package com.suminjin.simpleroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleroomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/suminjin/simpleroom/SimpleroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suminjin.simpleroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
